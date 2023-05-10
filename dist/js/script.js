853b58d696854af0166368f8510da6a4d8514121
McastIII <jeancapunocapuno@gmail.com>
2023-05-10 15:45:15 +0800

    fourth commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,4 @@
+// toggle menu
 const toggleMenu = document.querySelector(".toggle__menu");
 const mobileNav = document.querySelector(".header__nav");
 const blurr = document.querySelector(".blur");
@@ -6,3 +7,28 @@ toggleMenu.addEventListener("click", () => {
     mobileNav.classList.toggle("open");
     blurr.classList.toggle("open");
 });
+
+// dropdown
+const links = document.querySelectorAll(".links__drop__item h4");
+
+links.forEach((baba) => {
+    baba.addEventListener("click", () => {
+    baba.nextElementSibling.classList.toggle("open");
+    baba.querySelector("i").classList.toggle("open");
+    });
+});
+
+
+// // header scroll
+// const header = document.querySelector(".header");
+// const brandimg = document.querySelector(".branding img");
+// window.addEventListener("scroll", () => {
+//     console.log(window.pageYOffset);
+//     if (window.pageYOffset >= 100) {
+//     header.classList.add("blue");
+//     brandimg.style = "background-color:red";
+//     } else {
+//     header.classList.remove("blue");}
+
+// });
+
